d3298db68191406cd123ed964af08873a8b25b87
Nilvando <nilvandopolicarpo@gmail.com>
2024-04-10 16:52:29 -0300

    Cores aleatórias

---
+++ lib/apf1_funcoes/ap1.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Ap1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cor Aleatória',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RandomColorPage(),
+    );
+  }
+}
+
+class RandomColorPage extends StatefulWidget {
+  @override
+  _RandomColorPageState createState() => _RandomColorPageState();
+}
+
+class _RandomColorPageState extends State<RandomColorPage> {
+  Color _textColor = Colors.black;
+
+  void _mudarCor() {
+    setState(() {
+      _textColor = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+          .withOpacity(1.0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cor Aleatória'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Toque no botão para mudar a cor',
+              style: TextStyle(fontSize: 20.0, color: _textColor),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _mudarCor,
+              child: const Text('Sortear cor'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_atividades/apf1_funcoes/ap1.dart';
+
+void main() {
+  runApp(Ap1());
+}
